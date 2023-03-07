669a7b1571c56beec4907c1a21bcadd59e272385
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-07 09:11:02 -0700

    add Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,12 +3,12 @@ require './lib/item'
 RSpec.describe Item do
   before(:each) do
     @item1 = Item.new({name: 'Peach', price: '$0.75'})
-    @item1 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   it 'exists and has attributes' do
     expect(@item1).to be_a(Item)
     expect(@item2.name).to eq('Tomato')
-    expect(@item2.price).to eq('0.5')
+    expect(@item2.price).to eq(0.5)
   end
 end
