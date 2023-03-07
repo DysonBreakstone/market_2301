73843a9fac442eccb3e84b24643a864d8437a660
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-07 11:02:58 -0700

    add tests for selling items

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -98,4 +98,48 @@ RSpec.describe Market do
                                               "Cow tails"=>{quantity: 70, vendors: [@vendor1, @vendor2]}})
     end
   end
+
+  describe "Date and selling items" do
+    before(:each) do
+      @market1 = Market.new("South Pearl Street Farmers Market")
+      @vendor1 = Vendor.new("Rocky Mountain Fresh")
+      @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      @vendor3 = Vendor.new("Palisade Peach Shack")
+      @item1 = Item.new({name: "Peach", price: "$0.75"})
+      @item2 = Item.new({name: "Tomato", price: "$0.50"})
+      @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      @item5 = Item.new({name: "Pez dispenser", price: "$4.50"})
+      @item6 = Item.new({name: "Cow tails", price: "$1.50"})
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor1.stock(@item6, 35)
+      @vendor2.stock(@item3, 25)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item6, 35)
+      @vendor3.stock(@item1, 65)
+      @vendor3.stock(@item5, 80)
+      @vendor3.stock(@item2, 13)
+      @vendor3.stock(@item4, 20)
+      @market1.add_vendor(@vendor1)
+      @market1.add_vendor(@vendor2)
+      @market1.add_vendor(@vendor3)
+    end
+
+    it "has a date" do
+      expect(@market1.date).to eq(Time.now.strftime("%d/%m/%Y"))
+    end
+
+    it "can sell items" do
+      expect(@market1.sell(@item2, 1000)).to eq(false)
+
+      @market.sell(@item4, 65)
+      @market.sell(@item6, 60)
+
+      expect(@vendor2.inventory[@item4]).to eq(0)
+      expect(@vendor3.inventory[@item4]).to eq(5)
+      expect(@vendor1.inventory[@item6]).to eq(0)
+      expect(@vendor2.inventory[@item6]).to eq(10)
+    end
+  end
 end
